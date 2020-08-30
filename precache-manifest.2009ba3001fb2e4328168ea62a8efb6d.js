96c1aac8ac06715ee7cfc49f7545fd30e417fdc5
Gayane Mirzoyan <gayusha@gmail.com>
2020-08-30 03:00:11 -0700

    Updates

---
+++ precache-manifest.2009ba3001fb2e4328168ea62a8efb6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1cc32b3dc8e5f205537d45d8a35b7e4",
+    "url": "/ToDoList_Redux/index.html"
+  },
+  {
+    "revision": "252f49592e2b7fe0ef97",
+    "url": "/ToDoList_Redux/static/css/main.6a14c4cb.chunk.css"
+  },
+  {
+    "revision": "5f44b2d310d35b521829",
+    "url": "/ToDoList_Redux/static/js/2.5066d52a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDoList_Redux/static/js/2.5066d52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252f49592e2b7fe0ef97",
+    "url": "/ToDoList_Redux/static/js/main.42710113.chunk.js"
+  },
+  {
+    "revision": "1be1e1dfd256c62eca71",
+    "url": "/ToDoList_Redux/static/js/runtime-main.d0d45592.js"
+  }
+]);
